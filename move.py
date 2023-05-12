5b3faa5cbcece7bde73faee4e05f553fa70b98b4
ae20006shibaura <131850849+ae20006shibaura@users.noreply.github.com>
2023-05-13 08:16:27 +0900

    Update move.py

--- move.py
+++ move.py
@@ -1,10 +1,35 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import copy
+import os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import datasets,transforms
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-class ResNet_Dataset_val(torch.utils.data.Dataset):
-    def__init__(self,transform_RGB=None,transform_GLAY=None):
+from sklearn.metrics import f1_score, confusion_matrix, roc_curve, auc
 
-    RGB_image_names=os.listdir("./used_dataset/val/RGB/")
-    depth
+import warnings
+data = []
+
+path_ = 'Users/himamura6/Documents/GitHub/Wearing-a-mask-or-not' 
+
+for num in os.listdir(path_):
+    for img_path in os.listdir(f'{path_}/{num}')[:100]:
+        data.append([f'{path_}/{num}/{img_path}', img_path, num, num])
+
+df = pd.DataFrame(data, columns=['path', 'filename','category', 'label'])
+
+df.head()
+
+plt.imshow(img_transformed.numpy().transpose((1, 2, 0)))
+plt.show()
+class Wearing_Dataset_val(torch.utils.data.Dataset):
+   def __init__(self, df, features, labels):
+        self.features_values = df[features].values
+        self.labels = df[labels].values
